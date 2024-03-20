a90311c5094057443a17e3c83c0b3a9e5068eddb
Сергей Дубров <tmpvarcher@outlook.com>
2024-03-20 22:49:50 +0500

    Home Page, Page 404 and Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Header from "./components/header/Header";
+import HomePage from "./components/pages/home_page/HomePage";
+import PageNotFound from "./components/pages/page_not_found/PageNotFound";
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route element={<Header/>}>
+            <Route element={<HomePage/>} path='/'/>
+            <Route element={<PageNotFound/>} path='*'/>
+        </Route>
+    )
+);
+
+export default function App() {
 
     return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <>
+            <RouterProvider router={router}/>
+        </>
+    );
 }
-
-export default App
---
+++ src/components/pages/page_not_found/PageNotFound.tsx
@@ -0,0 +1,13 @@
+
+
+export default function PageNotFound() {
+    return (
+        <div>
+            <div>
+                <h1>Ошибка 404</h1>
+                <h2>Страница не найдена</h2>
+                <p>Выпейте чаю</p>
+            </div>
+        </div>
+    )
+}
