7bed1c6c57aa862d1be4a3a8b70c2f3c3ab9895b
Елена Аксенова <kittrisprior@gmail.com>
2024-05-03 17:51:41 +0500

    services added

--- src/pages/owner/ServicesInfo.tsx
+++ src/pages/owner/ServicesInfo.tsx
@@ -1,10 +1,23 @@
-import { List } from "antd";
+import { List, Button } from "antd";
 import { Service } from "../../components/types";
 import { FC } from "react";
 import { NoData } from "../ux/NoData";
+import { useNavigate } from "react-router-dom";
+import { useServiceQuery } from "../../components/api/serviceApi";
+import { Guid } from "guid-typescript";
 
 export const ServicesInfo : FC<{ data: Service[] | undefined}> = ({data}) => {
-    console.log(data)
+
+    const navigate = useNavigate()
+
+    const handleServiceEdit = () => {
+        // const query = useServiceQuery(id?.toString() || '')
+        // const { data: services} = query
+        navigate('../service-adding')
+    }
+
+    const handleServiceDelete = (evt : any) => {
+    }
     return (
         <div>
         {data?.length? 
@@ -16,6 +29,14 @@ export const ServicesInfo : FC<{ data: Service[] | undefined}> = ({data}) => {
                     <List.Item.Meta title={item.name} />
                     <List.Item.Meta title={item.price + " р."}/>
                     <List.Item.Meta title={item.duration + " мин."}/>
+                    <List.Item.Meta
+                        title={
+                            <Button onClick={handleServiceEdit}>Редактировать</Button>
+                    }/>
+                    <List.Item.Meta 
+                        title={
+                        <Button onClick={handleServiceDelete}>Удалить</Button>
+                    }/>
                 </List.Item>
             )}
             /> : <p className="no-services">
