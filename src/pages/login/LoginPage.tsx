d4ddb14ba16a7d6f3f6085ead6d711df0a149745
Сергей Дубров <tmpvarcher@outlook.com>
2024-05-13 15:25:07 +0500

    Начальная реализация авторизации

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,36 +1,38 @@
 import { Input, Button } from "antd";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { fakeAPI } from "../../components/fake-api/fakeApi";
-import ky from 'ky';
+import React, { useState } from "react";
+import { useAuth0 } from '@auth0/auth0-react';
 
 // Не работает
 export default function LoginPage() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const navigate = useNavigate();
+    const { loginWithRedirect, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
 
-    const onSubmit = async (data) => {
-        try {
-            // Use Ky to send the login request
-            const response = await ky.post(fakeAPI + 'users/', {
-                json: data,
-            });
-            if (!response.ok) {
-                throw new Error('Login failed');
-            }
-            navigate('/');
-        } catch (error) {
-            console.error('There has been a problem with your login operation:', error);
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if(!isAuthenticated) {
+            loginWithRedirect();
+            return
         }
+
+        try {
+            const token = await getAccessTokenSilently();
+            console.log(token);
+        } catch (e) {
+            console.error("Ошибка при получении токена", e);
+        }
+
     };
 
     return (
         <>
             <div>
                 <h2>Войти в аккаунт</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input type='text' placeholder='Почта или телефон' {...register("email", { required: true, pattern: /^[A-Za-z0-9]+$/i })}/>
-                    <Input type='password' placeholder='Пароль' {...register("password", { required: true })}/>
+                <form onSubmit={handleSubmit}>
+                    <Input type='text' value={email} placeholder='Почта или телефон' onChange={(e) => setEmail(e.target.value)} required/>
+                    <Input type='password' value={username} placeholder='Пароль' onChange={(e) => setUsername(e.target.value)} required />
                     <Button className="profile-page-button" type="submit">Войти</Button>
                 </form>
                 <NavLink to="/sign-up">Нет аккаунта? Создайте его!</NavLink>
